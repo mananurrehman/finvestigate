e2fb3ea0cd273233306015f99fc79b10efab5d35
mananurrehman <mananurrehman@gmail.com>
2026-04-06 16:42:24 +0500

    host added in run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ config_name = os.getenv("FLASK_ENV", "development")
 app = create_app(config_name)
 
 if __name__ == "__main__":
-    app.run(debug=app.config.get("DEBUG", False))
+    app.run(host="0.0.0.0", port=5000, debug=app.config.get("DEBUG", False))
